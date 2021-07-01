9aa8ef35d66f6e251220af644252d144c069d990
Rasheeda Alexander <rasheedaalexander@sheedas-mbp.fios-router.home>
2021-07-01 17:17:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Avatar - The Last Airbender is one of my favorite shows. Let's learn more about the characters.
+
+This app was built using ReactJS and includes a search function.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Avatar - The Last Airbender</h1>
+    </div>
+  );
+}
+
+export default App;
