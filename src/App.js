1597357706adde0d566954b581971761645e8004
Rasheeda Alexander <rasheedaalexander@sheedas-mbp.fios-router.home>
2021-07-08 00:07:05 -0400

    displayed character names from the show in a list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { useState , useEffect} from 'react';
+import axios from "axios";
+import Header from './components/Header';
+import Character from './components/Character'
 import './App.css';
+// import { assertExpressionStatement } from '@babel/types';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+const fetchItems = async () => {
+  const result = await axios(`https://last-airbender-api.herokuapp.com/api/v1/characters`);
+  
+  console.log(result.data);
+
+  setItems(result.data);
+  setIsLoading(false);
+}
+
+fetchItems();
+}, [])
+
   return (
     <div className="container">
-      <h1>Avatar - The Last Airbender</h1>
+      <Header />
+      <Character isLoading={isLoading} items={items}/>
     </div>
   );
 }
